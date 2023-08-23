5a35948adf71d7518bab00135c1336049e6c01ae
CamilaPortal <c.portal@alumno.um.edu.ar>
2023-08-23 11:11:18 -0300

    First commit

---
+++ tests/test_app.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestMulti(unittest.TestCase):
+    def test_multi(self):
+        self.assertEqual(multiplicacion(2,3), 6)
+
+
+if __name__== '__main__':
+    unittest.main()
